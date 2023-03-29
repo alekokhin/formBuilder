d3a49e5901c83ca55d1da43990a974d305e571d9
alekokhin <alekokhinikadze@gmail.com>
2023-03-30 01:35:20 +0400

    Read data from a json file

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-fetch('./form.json')
+/**import json file */
+fetch("./form.json")
   .then(response => response.json())
   .then(data => {
-    console.log(data);
+    /**main element */
+    const mainElement = document.getElementById('maindiv')
+    //**loop for each element */
+    data.properties.forEach(property => {
+      let element
+      element = document.createElement('input')
+      property.type === 'boolean' ? element.setAttribute('type', "checkbox") : element.setAttribute('type', property.type)
+      element.setAttribute('name', property.name)
+      element.setAttribute('placeholder', property.label)
+      if (property.required) {
+        element.setAttribute('required', property.required);
+      }
+      if (property.inputType) {
+        element.setAttribute('type', property.inputType)
+      }
+      if (property.pattern) {
+        element.setAttribute('pattern', property.pattern)
+      }
+      if (property.minLength && property.maxLength) {
+        element.setAttribute('minLength', property.minLength)
+        element.setAttribute('maxLength', property.maxLength)
+      }
 
-  
-})
-  .catch(error => {
-    console.error('Error fetching JSON data', error);
-  });
+      //add element 
+      mainElement.append(element)
+
+    })
+    /**submit button */
+    const submitButton = document.createElement('input');
+      submitButton.setAttribute('type', 'submit');
+      submitButton.textContent = 'Submit';
+mainElement.append(submitButton);
+  })
+  .catch(error => console.error(error))
